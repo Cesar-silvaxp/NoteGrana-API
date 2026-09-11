915b2f3e4cae32fa5a95ba023f8c4dc1e1b369ee
César Silva <cesarleal632@gmail.com>
2026-09-11 15:06:16 -0300

    Inicializa API NoteGrana com Spring Boot

---
+++ src/main/java/com/notegrana/api/NotegranaApiApplication.java
@@ -0,0 +1,13 @@
+package com.notegrana.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotegranaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotegranaApiApplication.class, args);
+	}
+
+}
